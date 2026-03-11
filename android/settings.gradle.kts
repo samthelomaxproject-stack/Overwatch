bc7bb878fd0933af0ebae22f53e714fd2932ad58
Sam The Lomax Project <sam.thelomaxproject@gmail.com>
2026-03-11 06:16:02 -0500

    Fix DAT package auth: provide GitHub Packages username (GITHUB_ACTOR) with token

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -20,7 +20,7 @@ dependencyResolutionManagement {
         maven {
             url = uri("https://maven.pkg.github.com/facebook/meta-wearables-dat-android")
             credentials {
-                username = ""
+                username = System.getenv("GITHUB_ACTOR") ?: localProperties.getProperty("github_user") ?: "x-access-token"
                 password = System.getenv("GITHUB_TOKEN") ?: localProperties.getProperty("github_token")
             }
         }
